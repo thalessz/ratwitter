e01019606282280d385f5c1c533df1bf53b2ff17
thalessz <thalessouza1904@gmail.com>
2024-11-23 00:15:12 -0300

    [/] / added on route

--- ratwitter/app/src/main/java/com/thalessz/ratwitter/api/js/src/routes/userRoutes.js
+++ ratwitter/app/src/main/java/com/thalessz/ratwitter/api/js/src/routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController=require('../controllers/userController');
 
 router.post('/login', userController.login);
 router.post('/cadastro', userController.cadastro);
-router.get('/fetch_user:id', userController.fetchByID);
+router.get('/fetch_user/:id', userController.fetchByID);
 router.get('/fetch_uid/:username', userController.fetchID);
 
 module.exports = router;
